5f2f8f8c68f44f4ada45c179bc9ed2b91a598210
Mateusz <mateuszrorat@gmail.com>
2016-03-07 16:22:19 +0100

    chore: added express server for heroku

---
+++ server/server.js
@@ -0,0 +1,10 @@
+// Import required modules.
+var express = require('express');
+
+// Initialize our Express app.
+var app = express();
+
+// Listen for incoming requests and serve them.
+app.listen(process.env.PORT || 3002);
+
+app.use(express.static('.tmp'));
