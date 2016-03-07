55e6d2f1ff2d9d755ff0eb18487214038883c8d7
Mateusz <mateuszrorat@gmail.com>
2016-03-07 17:01:41 +0100

    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import required modules.
 var express = require('express');
+var path = require('path');
 
 // Initialize our Express app.
 var app = express();
@@ -7,5 +8,5 @@ var app = express();
 // Listen for incoming requests and serve them.
 app.listen(process.env.PORT || 3002);
 
-console.log(__dirname, __dirname + '/.tmp', 'tu');
-app.use(express.static('./.tmp'));
+console.log(__dirname, __dirname + '/.tmp', 'tu', path.join(__dirname, 'public/stylesheets'));
+app.use(express.static(path.join(__dirname, '.tmp')));
