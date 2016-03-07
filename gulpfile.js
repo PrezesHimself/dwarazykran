c883a5f31bfdb039c458ce62038788ab8398b74a
Mateusz <mateuszrorat@gmail.com>
2016-03-07 12:35:41 +0100

    chore: better build WIP

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,50 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var modRewrite  = require('connect-modrewrite');
 var shell = require('gulp-shell')
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var rimraf = require('gulp-rimraf');
 
-gulp.task('default', ['browser-sync', 'cors'], function() {
+gulp.task('clean', function() {
+    return gulp.src('dist', { read: false })
+        .pipe(rimraf({ force: true }));
+});
+
+gulp.task('default', ['build', 'browser-sync', 'cors'], function() {
 
 })
 
+gulp.task('dist', ['clean'], function() {
+    return gulp.src('app/index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', ngAnnotate()))
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('js', function() {
+    return gulp.src('app/index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', ngAnnotate()))
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulp.dest('.tmp'));
+});
+
 // Static server
+gulp.task('build', ['js'], function() {
+
+})
+
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
-            baseDir: "./",
+            baseDir: ".tmp",
             middleware: [
                 modRewrite([
-                    '!\\.\\w+$ /app/index.html [L]'
+                    '!\\.\\w+$ index.html [L]'
                 ])
             ]
         }
@@ -24,7 +55,7 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function () {
     gulp.watch(['./app/*.html'], ['reload']);
     gulp.watch(['./app/*.css'], ['reload']);
-    gulp.watch(['./app/*.js'], ['reload']);
+    gulp.watch(['./app/*.js'], ['js', 'reload' ]);
 });
 
 gulp.task('cors', shell.task([
