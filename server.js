77b65ba9c4588ced804612be85760d0658cee8b5
Mateusz <mateuszrorat@gmail.com>
2016-03-07 17:08:56 +0100

    test

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var path = require('path');
 // Initialize our Express app.
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 // Listen for incoming requests and serve them.
 app.listen(process.env.PORT || 3002);
-
-console.log(__dirname, __dirname + '/.tmp', 'tu', path.join(__dirname, '../.tmp'));
-app.use(express.static(path.join(__dirname, '.tmp')));
