10d4a9f86ff6f1c933026cb1a912149a0e4ae6fe
Mateusz <mateuszrorat@gmail.com>
2016-03-07 17:05:28 +0100

    conf

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ var app = express();
 // Listen for incoming requests and serve them.
 app.listen(process.env.PORT || 3002);
 
-console.log(__dirname, __dirname + '/.tmp', 'tu', path.join(__dirname, 'public/stylesheets'));
-app.use(express.static(path.join(__dirname, '../.tmp')));
+console.log(__dirname, __dirname + '/.tmp', 'tu', path.join(__dirname, '../.tmp'));
+app.use(express.static(path.join(__dirname, '.tmp')));
